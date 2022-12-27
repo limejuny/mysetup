f6f7f7e1b2b4db5218c7b4590753f72888156525
Junyeong Yim <limejuny@gmail.com>
2022-12-27 22:05:21 +0900

    coc-tsserver 설치
    Detail
    ======
     - js, ts autocomplete

--- ansible/roles/vim/files/coc.sh
+++ ansible/roles/vim/files/coc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vi +'CocInstall -sync coc-html coc-json coc-xml coc-yaml coc-sh coc-pyright coc-java coc-clangd coc-solargraph' +qall
+vi +'CocInstall -sync coc-html coc-json coc-xml coc-yaml coc-sh coc-pyright coc-java coc-clangd coc-solargraph coc-tsserver' +qall
