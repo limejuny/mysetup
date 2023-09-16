96aad8f99f6649833d4c900c94a919325579f4c5
Junyeong Yim <lime@juny.me>
2023-09-16 23:39:17 +0900

    jenkins autocomplete
    Detail
    ======
     -

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -154,6 +154,9 @@ require('lazy').setup({
     'neoclide/coc.nvim',
     branch = 'release',
   },
+  'neovim/nvim-lspconfig',
+  'hrsh7th/nvim-cmp',
+  'joshzcold/cmp-jenkinsfile',
 
   -- Language support
   -- Go
@@ -527,6 +530,61 @@ vim.api.nvim_set_keymap('n', '<leader>gb', ':Git blame<CR>', { noremap = false, 
 
 -- Completion {{{
 vim.opt.completeopt = vim.opt.completeopt + 'longest'
+
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+cmp.setup.filetype({ "Jenkinsfile", "groovy" }, {
+  sources = {
+    {
+      name = "jenkinsfile",
+      option = {
+        jenkins_url = "https://jenkins.co",
+      },
+    },
+  },
+})
+
 -- }}}
 
 -- Formatter {{{
