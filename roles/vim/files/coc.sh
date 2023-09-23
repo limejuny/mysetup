5f2402d0966ba5f231a25359b1f0974f14c6ca0e
Junyeong Yim <lime@juny.me>
2023-09-23 09:43:54 +0900

    롬복 coc디렉토리에 설치
    Detail
    ======
     -

--- roles/vim/files/coc.sh
+++ roles/vim/files/coc.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 vi +'CocInstall -sync coc-html coc-json coc-xml coc-yaml coc-sh coc-pyright coc-java coc-clangd coc-solargraph coc-tsserver coc-tabnine' +qall
+mkdir -p ~/.config/coc/extensions/node_modules/coc-java/lombok
+cp ~/bin/lombok/lombok.jar ~/.config/coc/extensions/node_modules/coc-java/lombok/lombok.jar
